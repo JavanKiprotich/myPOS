18f4e2c63e246037a439215ee86cb640fadfec66
Javan Kiprotich <javokipz@gmail.com>
2026-07-23 16:20:56 -0700

    Fix TypeScript errors and update audit page

--- types/cart.ts
+++ types/cart.ts
@@ -5,25 +5,4 @@ export interface CartItem {
   price: number;
   quantity: number;
   subtotal: number;
-
-  defaultPaymentMethod: string;
-
-requireFullPayment: boolean;
-
-mpesaEnvironment: string;
-
-mpesaShortcode: string;
-
-mpesaTillNumber: string;
-
-mpesaConsumerKey: string;
-
-mpesaConsumerSecret: string;
-
-mpesaPasskey: string;
-
-mpesaCallbackUrl: string;
-
-mpesaAutoVerify: boolean;
 }
-
